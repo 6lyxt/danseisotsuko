8164b3c065f19814eb3dba438a0b3fe8a44c7831
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-06-24 09:27:14 +0200

    not needed

--- scripts/node_/configs.js
+++
@@ -1,8 +0,0 @@
-function saveConfig() {
-    const fs = require('fs');
-    fs.readFile("../gamedata/scenes/sceneSaver.json", (err, data) => {
-        if (err) return err;
-
-        return data;
-    });
-}
